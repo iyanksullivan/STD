54b7218dbc742b6e6a2c260ce763933d5c4fe139
iyanksullivan <32774837+iyanksullivan@users.noreply.github.com>
2019-04-05 16:28:01 +0700

    ReMerge

---
+++ funcpros.h
@@ -0,0 +1,44 @@
+//
+//  funcpros.h
+//  Tubes
+//
+//  Created by Iyank on 4/3/19.
+//  Copyright © 2019 Iyank. All rights reserved.
+//
+
+#ifndef funcpros_h
+#define funcpros_h
+#include <iostream>
+#define first(L) L.first
+#define last(L) L.last
+#define judul(P) P->judul
+#define artik(P) P->artik
+#define tag(P) P->tag
+#define next(P) P->next
+#define prev(P) P->prev
+using namespace std;
+
+typedef struct data *address;
+
+struct data{
+    string judul;
+    string artik;
+    string tag;
+    address next;
+    address prev;
+};
+
+struct list{
+    address first;
+    address last;
+};
+
+bool isEmpty(list &L);
+void createlist(list &L);
+void createartikel(address &P, string jdl, string isi, string ta);
+void addartikel(list &L, address P);
+void cariartik(list L, string art);
+void tagterbanyak(list L, string tagart);
+void delartikel(list &L, address &P);
+void lihatartikel(list L);
+#endif /* funcpros_h */
---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tubes
+//
+//  Created by Iyank on 4/3/19.
+//  Copyright © 2019 Iyank. All rights reserved.
+//
+
+#include "funcpros.h"
+
+int main(){
+    cout<<"ATHALLA TAI"<<endl;
+    return 0;
+}
